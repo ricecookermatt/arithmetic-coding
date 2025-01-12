398c9783e73a575905b09ff096a6ce0db8d73133
matthew <28170465+ricecookermatt@users.noreply.github.com>
2025-01-12 00:20:57 -0800

    Script to calculate minimum theoretical and empirical number of bits required for compression

---
+++ arithmetic_coding_empirical_bits.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 11 15:14:13 2025
+
+@author: matthew
+
+This Python script empirically determines the maximum number of bits required
+when performing arithmetic coding (decimal codeword to binary conversion).
+
+
+To put into Jupyter Notebook:
+Constraints:
+    Number of total characters in the data is a power of 2
+    Number of unique characters is no more than 4
+    Each character is i.i.d.
+    
+Theorized bound:
+    Assuming worst case the data only contains characters that occur once,
+    the encoding will perform N times on the same delta (which is equal to 
+    the p.m.f.).  
+    
+    Where N = data length, M = # unique chars
+    In the scenario described above, N = M and delta (pmf) is equal to 1/M
+
+    -log2[ (1/M)^N ]
+
+    Sign negation is due to pmf being fractional
+    Use log2 since converting to binary
+    
+    
+    Since the data is encoded in the middle between the bounds, an extra bit
+    is required
+    
+    Theorized worst case number of bits: 1 + -log2[ (1/M)^N ] 
+    To make more intuitive with the addition of the extra bit:
+        
+    1 + log2[ (1/M)^(-N) ]
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from arithmetic_coding import compute_pmf, compute_cdf, decimal_encode, binary_encode
+
+
+#Testing begins here
+#-----------------------------------------------------------------------------
+#Num of combinations (assuming order matters): M^N
+#N = data length, M = number of symbols
+
+num_unique_symbols = 8
+symbols = np.arange(num_unique_symbols)
+
+data_length = 8
+
+theoretical_bound = np.int64(1 + np.log2( data_length**(num_unique_symbols) ) )
+print("Theoretical bound:", theoretical_bound, "bits")
+
+
+#Maximum number of combinations
+max_sims = num_unique_symbols**data_length
+num_bits = np.zeros(max_sims, dtype='int64')
+
+#Create generator object
+rng = np.random.default_rng()
+
+
+for i in range(max_sims):
+    #Randomize data using discrete uniform distribution
+    data = rng.integers(0, max(symbols), data_length, endpoint=True)
+    
+    pmf = compute_pmf(data)
+    cdf = compute_cdf(pmf)
+    dec_code = decimal_encode(data, pmf, cdf)
+    bin_code = binary_encode(dec_code)
+    
+    min_bits = 1 + bin_code.rfind('1')
+    num_bits[i] = min_bits
+    
+    
+print("Empirical bound:", max(num_bits), "bits")
+
+#Minimum number of bits will be 1 for the case where data is all 0's or 1's
+#Need to add +2 to end of np.arange() because: 1) up to but not including
+#1) up to but not including caused by arange()
+#2) up to but not including caused by histogram()
+bin_edges = np.arange(1, 25+1+1)
+plt.hist(num_bits, bins=bin_edges, density=True, align='left', edgecolor = 'k')
+
+plt.xlabel("Minimum number of bits")
+plt.ylabel("Probability")
+
+title = f"Empirical Results from {max_sims} Simulations"
+plt.title(title)
+
+plt.grid()
+plt.minorticks_on()
+
+plt.xlim([0, 26])
+plt.ylim([0, 1])
+
+plt.show()
